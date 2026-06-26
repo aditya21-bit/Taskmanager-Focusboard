fc4a1ce260e6b21c8c014ba28c6a01f9fcef74eb
aditya21-bit <guptaadityaa21@gmail.com>
2026-06-26 14:23:29 +0530

    Configure production of API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Automatically send token
-API.interceptors.request.use((req) => {
-
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
-  return req;
+  return config;
 });
 
 export default API;
